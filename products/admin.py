aa52f30485776cf45cdf40e235c7e61951093920
Abigail Ryan <abi.ryan91@gmail.com>
2024-12-19 20:24:29 +0000

    Add customisation to admin panel models

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,41 @@ from django.contrib import admin
 from .models import Product, Category, Brand, SkinType
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(SkinType)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'brand',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class SkinTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(SkinType, SkinTypeAdmin)
