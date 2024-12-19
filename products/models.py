70d7d1f9853a3ffc2837bb6e3684de3de1c6d9ed
Abigail Ryan <abi.ryan91@gmail.com>
2024-12-19 20:12:38 +0000

    Add products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class SkinType(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    skin_type = models.ForeignKey('SkinType', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    product_size_ml = models.IntegerField(null=True, blank=True)
+    product_size_g = models.IntegerField(null=True, blank=True)
+    key_ingredients = models.TextField(null=True, blank=True)
+    ingredients = models.TextField(null=True, blank=True)
+    how_to_use = models.TextField(null=True, blank=True)
+    vegan = models.BooleanField(default=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
