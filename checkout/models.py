be41ee444d77dc7d574c7fb88b6891bd48cf074e
Abigail Ryan <abi.ryan91@gmail.com>
2025-01-01 23:39:11 +0000

    Remove product size fields from orderlineitem model

--- checkout/models.py
+++ checkout/models.py
@@ -56,8 +56,6 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size_ml = models.IntegerField(null=True, blank=True)
-    product_size_g = models.IntegerField(null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
