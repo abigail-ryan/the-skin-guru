c185c6280843583e7d4b4237b1b511dce8d4ad48
Abigail Ryan <abi.ryan91@gmail.com>
2025-01-04 23:22:07 +0000

    Add image field to post model

--- articles/models.py
+++ articles/models.py
@@ -19,6 +19,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    image = models.ImageField(null=True, blank=True)
 
     class Meta:
         ordering = ["-created_on"]
