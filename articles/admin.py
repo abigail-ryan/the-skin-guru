f69ffb37d770c2d6b3f8ed867f74a0a0d4cd0f1a
Abigail Ryan <abi.ryan91@gmail.com>
2025-01-04 18:31:35 +0000

    Ad customisation to admin panel for posts & comments

--- articles/admin.py
+++ articles/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Post, Comment
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status','created_on', 'updated_on',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('author', 'body', 'approved', 'created_on')
+    list_filter = ('approved','updated_on',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
