8591f0df548d5cb9ef01a260b4346f9c1aa06f09
Abigail Ryan <abi.ryan91@gmail.com>
2025-01-06 04:05:18 +0000

    Add shop by and article content to index.html

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from articles.models import Post
 
 # Create your views here.
 
 def index(request):
     """
-    A view to return the index page
+    A view to return the index page with article listings
     """
 
-    return render(request, 'home/index.html')
+    latest_posts = Post.objects.filter(status=1).order_by('-created_on')[:4]
+    context = {
+        'latest_posts': latest_posts,
+    }
+
+    return render(request, 'home/index.html', context)
