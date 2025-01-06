560aafd2ac15d9127ae8ea0ced7063c046b1ade3
Abigail Ryan <abi.ryan91@gmail.com>
2025-01-06 23:45:53 +0000

    Add postgresql database

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn the_skin_guru.wsgi:application
--- the_skin_guru/settings.py
+++ the_skin_guru/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +31,7 @@ SECRET_KEY = 'django-insecure-8ay4c+g0$&we+3f7@uf9t2bq%d)k&o!5(_tmp0q(61wxv--)ux
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-abigailryan-theskinguru-yssz3vpl1n6.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-abigailryan-theskinguru-yssz3vpl1n6.ws.codeinstitute-ide.net', 'the-skin-guru.herokuapp.com']
 
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-abigailryan-theskinguru-yssz3vpl1n6.ws.codeinstitute-ide.net']
@@ -132,12 +134,17 @@ WSGI_APPLICATION = 'the_skin_guru.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
