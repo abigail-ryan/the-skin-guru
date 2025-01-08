c081941aa2cca2786c57c8c4bc3748bd15668726
Abigail Ryan <abi.ryan91@gmail.com>
2025-01-08 03:06:32 +0000

    Refactor contact app py files for pep8 validation

--- contact/admin.py
+++ contact/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import ContactForm
 
+
 # Register your models here.
 class ContactFormAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'message', 'sent_on', 'read',)
-    list_filter = ('sent_on','read',)
-  
+    list_filter = ('sent_on', 'read',)
 
 
 admin.site.register(ContactForm, ContactFormAdmin)
